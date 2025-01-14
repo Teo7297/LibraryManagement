f4489c04df15a2a71e68dc1ff76038d12d729979
Teo7297 <teocava7297@gmail.com>
2025-01-14 15:36:50 +0100

    Repo setup + rapidJson example

---
+++ include/LmsDefines.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// STL
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <sstream>
+
+// RapidJSON
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
